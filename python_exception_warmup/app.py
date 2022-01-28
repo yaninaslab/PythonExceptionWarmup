791631c6bcbeed565026ed50095ef897ad49e7c4
yaninaslab <yana.belash@gmail.com>
2022-01-27 21:58:42 -0500

    Working on objects in class

--- python_exception_warmup/app.py
+++ python_exception_warmup/app.py
@@ -1,12 +1,13 @@
 # TODO Please make me safe! There are so many places where I can fail.
 # import the choices from choices.py
+
 import choices
 
 # Function for running the function associated with the user choice
 
 
 def run_choice(index):
-    choices.options[index]['function']()
+    choices.options[index].action()
 
 
 # Print a nice little welcome message
@@ -16,12 +17,20 @@ print("First, you must choose your own adventure.")
 
 # Go through all choices and show each choice string to the user
 for i in range(len(choices.options)):
-    print(f"Option {i+1}: {choices.options[i]['string']}")
+    print(f"Option {i+1}: {choices.options[i].text}")
 
 # Show the user options until the user decides to choose the exit option
 while(True):
     # Ask for the user input, assume it's an integer
-    user_input = int(
-        input("Please select the number you would like to choose: "))
+    try:
+        user_input = int(
+            input("Please select the number you would like to choose: "))
+        run_choice(user_input - 1)
+    except ValueError as e:
+        print("You must enter an actual number!")
+    except IndexError:
+        print("Please select a valid option number!")
+    except:
+        print("Sorry, something went wrong!")
+        exit()
     # Call the run_choice function with the users choice - 1 (because arrays count from 0)
-    run_choice(user_input - 1)
--- python_exception_warmup/choices.py
+++ python_exception_warmup/choices.py
@@ -3,27 +3,47 @@ import jokes
 import addition
 import happy
 
+
+class UserOption:
+    def __init__(self, text, action):
+        self.text = text
+        self.action = action
+
+
+option_one = UserOption("Tell me a joke", jokes.tell_joke)
+option_two = UserOption("Let me add a couple numbers", addition.add_nums)
+option_three = UserOption("Show me a happy face", happy.print_happy_face)
+option_four = UserOption("Leave this place", exit)
+# print(option_one.text)
+# option_one.action
+
+
 # Each element of the array is a dictionary
 # Each dictionary contains a string that is show to the user and a function that is called if they pick it
 # Cool thing here is you can add more options here and they will automatically show up the next time you run the program.
 options = [
-    {
-        "string": "Tell me a joke",
-        "function": jokes.tell_joke
-    },
+    option_one,
+    option_two,
+    option_three,
+    option_four
 
-    {
-        "string": "Let me add a couple numbers",
-        "function": addition.add_nums
-    },
+    # {
+    #     "string": "Tell me a joke",
+    #     "function": jokes.tell_joke
+    # },
 
-    {
-        "string": "Show me a happy face",
-        "function": happy.print_happy_face
-    },
+    # {
+    #     "string": "Let me add a couple numbers",
+    #     "function": addition.add_nums
+    # },
 
-    {
-        "string": "Leave this place",
-        "function": exit
-    }
+    # {
+    #     "string": "Show me a happy face",
+    #     "function": happy.print_happy_face
+    # },
+
+    # {
+    #     "string": "Leave this place",
+    #     "function": exit
+    # }
 ]
